40755d2dd7fdc6a8f47bb17a7e2854f648065a46
Joshua Olilima <joshuaolilima@gmail.com>
2023-02-16 20:59:19 +0000

    preprocessor

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *  *main-Serves as the entry piont of a program
+ *   *initialize the strings
+ *    *write the strings out
+ *     *Return:if no error return zero, otherrwise return non zero value
+ *      */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
