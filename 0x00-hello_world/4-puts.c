b37de92cffd806704bd276fa3d0f0ded6ec8bc1f
Joshua Olilima <joshuaolilima@gmail.com>
2023-02-16 20:49:36 +0000

    preprocessor

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *  *  *main-Serves as the entry piont of a program
- *   *   *initialize the strings
- *    *    *write the strings out
- *     *     *Return:if no error return zero, otherrwise return non zero value
- *      */
+ *   main-Serves as the entry piont of a program
+ *   initialize the strings
+ *   write the strings out
+ *   Return:if no error return zero, otherrwise return non zero value
+ *   */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
