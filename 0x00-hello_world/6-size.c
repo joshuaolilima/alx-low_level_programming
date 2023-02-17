3463350b3aaea4b4ce0feb672ca2970ef3c3387d
Joshua Olilima <joshuaolilima@gmail.com>
2023-02-17 04:23:24 +0000

    preprossor

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 /**
- *  main-Serves as the entry piont of a program
- *  initialize the strings
- *  write the strings out
- *  Return:if no error return zero, otherrwise return non zero value
+ *  main -is the entry point for the program.
+ *  getting the size
+ *  Return:zero if no error, Non-zero value  if errors.
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
